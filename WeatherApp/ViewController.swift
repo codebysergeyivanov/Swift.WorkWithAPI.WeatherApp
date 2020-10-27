40e3619d7af8dfe3123b395d0e452759890426a3
Sergei Ivanov <100bb100hands@gmail.com>
2020-10-27 17:52:28 +0700

    init commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Сергей Иванов on 27.10.2020.
+//  Copyright © 2020 Сергей Иванов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
